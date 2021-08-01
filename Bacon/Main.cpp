7bef14b315c5e52425cc8254a2e600c64c65e437
Spectrum76 <49938052+Spectrum76@users.noreply.github.com>
2021-08-01 20:41:25 +0530

    Switched to glfw.

--- Bacon/Main.cpp
+++ Bacon/Main.cpp
@@ -1,20 +1,18 @@
 // Copyright (c) 2021 Rayvant. All rights reserved.
 
 #define WIN32_LEAN_AND_MEAN
+#define GLFW_EXPOSE_NATIVE_WIN32
+
 #include <Windows.h>
 
-#include <chrono>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 
 #include "Renderer.h"
 #include "Camera.h"
 #include "Model.h"
 
-using namespace std::chrono;
-
-#define GET_X_LPARAM(lp) ((int)(short)LOWORD(lp))
-#define GET_Y_LPARAM(lp) ((int)(short)HIWORD(lp))
-
-bool Keys[255];
+bool Keys[1024];
 
 double lastX;
 double lastY;
@@ -27,119 +25,97 @@ bool mouseFirstMoved = true;
 double deltaTime = 0.0f;
 double lastTime = 0.0f;
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void HandleKeys(GLFWwindow* window, int Key, int Code, int Action, int Mode);
+void HandleMouse(GLFWwindow* window, double xPos, double yPos);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+int main()
 {
 	const int WIDTH = 800;
 	const int HEIGHT = 600;
 
-	WNDCLASSEX windowClass = {};
-	windowClass.cbSize = sizeof(WNDCLASSEX);
-	windowClass.style = CS_HREDRAW | CS_VREDRAW;
-	windowClass.lpfnWndProc = WndProc;
-	windowClass.hInstance = hInstance;
-	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	windowClass.lpszClassName = L"WindowClass";
+	glfwInit();
 
-	RegisterClassEx(&windowClass);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	RECT windowRect = { 0, 0, WIDTH, HEIGHT };
-	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Bacon", nullptr, nullptr);
 
-	HWND hwnd = CreateWindow(
-		L"WindowClass",
-		L"Bacon",
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT,
-		windowRect.right - windowRect.left,
-		windowRect.bottom - windowRect.top,
-		nullptr, nullptr,
-		hInstance,
-		nullptr);
+	glfwSetKeyCallback(window, HandleKeys);
+	glfwSetCursorPosCallback(window, HandleMouse);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-	ShowWindow(hwnd, nCmdShow);
-
-	Renderer renderer = Renderer(hwnd);
+	Renderer renderer = Renderer(glfwGetWin32Window(window));
 
 	renderer.Init();
 
 	Model cube = Model(renderer.GetDevice(), renderer.GetContext());
-	Camera camera = Camera(hwnd, renderer.GetDevice(), renderer.GetContext());
+	Camera camera = Camera(glfwGetWin32Window(window), renderer.GetDevice(), renderer.GetContext());
 
 	cube.Load("cube.obj");
 
-	MSG msg = {};
-	while (msg.message != WM_QUIT)
+	while (!glfwWindowShouldClose(window))
 	{
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-		else
-		{
-			auto before = std::chrono::high_resolution_clock::now();
+		double now = glfwGetTime();
+		deltaTime = now - lastTime;
+		lastTime = now;
 
-			camera.KeyControl(Keys, (float)deltaTime);
-			camera.MouseControl((float)xChange, (float)yChange);
+		camera.KeyControl(Keys, (float)deltaTime);
+		camera.MouseControl((float)xChange, (float)yChange);
 
-			xChange = 0.0f;
-			yChange = 0.0f;
+		xChange = 0.0f;
+		yChange = 0.0f;
 
-			camera.CalculateViewMatrix();
+		camera.CalculateViewMatrix();
 
-			renderer.Render();
+		renderer.Render();
 
-			camera.Bind();
+		camera.Bind();
 
-			cube.Draw();
+		cube.Draw();
 
-			renderer.Update();
+		renderer.Update();
 
-			auto after = std::chrono::high_resolution_clock::now();
-
-			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(after - before);
-			deltaTime = (float)duration.count() / 1000000.0f;
-		}
+		glfwPollEvents();
 	}
 
-	return static_cast<char>(msg.wParam);
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return EXIT_SUCCESS;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+void HandleKeys(GLFWwindow* window, int Key, int Code, int Action, int Mode)
 {
-	switch (message)
+	if (Key == GLFW_KEY_ESCAPE && Action == GLFW_PRESS)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-
-	case WM_KEYDOWN:
-		Keys[wParam] = true;
-		break;
-
-	case WM_KEYUP:
-		Keys[wParam] = false;
-		break;
-
-	case WM_MOUSEMOVE:
-		int xPos = GET_X_LPARAM(lParam);
-		int yPos = GET_Y_LPARAM(lParam);
-
-		if (mouseFirstMoved)
-		{
-			lastX = xPos;
-			lastY = yPos;
-			mouseFirstMoved = false;
-		}
-
-		xChange = xPos - lastX;
-		yChange = lastY - yPos;
-
-		lastX = xPos;
-		lastY = yPos;
+		glfwSetWindowShouldClose(window, GL_TRUE);
 	}
 
-	return DefWindowProc(hWnd, message, wParam, lParam);
+	if (Key >= 0 && Key < 1024)
+	{
+		if (Action == GLFW_PRESS)
+		{
+			Keys[Key] = true;
+		}
+		else if (Action == GLFW_RELEASE)
+		{
+			Keys[Key] = false;
+		}
+	}
+}
+
+void HandleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	if (mouseFirstMoved)
+	{
+		lastX = xPos;
+		lastY = yPos;
+		mouseFirstMoved = false;
+	}
+
+	xChange = xPos - lastX;
+	yChange = lastY - yPos;
+
+	lastX = xPos;
+	lastY = yPos;
 }
